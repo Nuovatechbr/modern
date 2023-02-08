f0654ef12e30b541bf05613fe616b5737b13210b
Eduardo Marinho <carlos@compuvox.com.br>
2023-02-07 21:56:32 -0300

    Mudança no layout

---
+++ core/Navigation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Nuovatech\Template\Modern;
+
+abstract class Navigation
+{
+
+    /**
+     * Store the menu object
+     * @var array
+     */
+    private static $nav = [];
+
+    public static function render()
+    {
+        $list = '';
+        foreach (self::$nav as $option) {
+
+            // Cria o link para adicionar no objeto de lista
+            if ($option->qtdSubMenu > 0) {
+                $link   = "<a class='modern-navigation-parent' title='$option->title'>$option->label<i class='modern-navigation-parent-arrow icon-arrow-down'></i></a>";
+                foreach (self::$nav as $key => $sub) {
+
+                    if ($sub->idMenuParent == $option->idMenu) {
+                        $link .= "<a class='modern-navigation-child' href='$sub->url' title='$sub->title'>$sub->label</a>";
+                    }
+                }
+                $li     = "<li class='modern-navigation-li-parent'>$link</li>";
+            } else {
+                $link   = "<a class='modern-navigation-child' href='$option->url' title='$option->title'>$option->label</a>";
+                $li     = "<li class='modern-navigation-li'>$link</li>";
+            }
+
+            $list   .= $li;
+        }
+
+        $ul     =   "<ul class='modern-navigation-ul'>$list</ul>";
+        $nav    =   "<nav class='modern-navigation'>$ul</nav>";
+        print_r($nav);
+    }
+
+    /**
+     * Método responsável por definir as opções da navegação
+     * @param   array $navigation array contendo informações da navegação.
+     */
+    public static function setNavigation(array $nav = [])
+    {
+        self::$nav = $nav;
+    }
+}
